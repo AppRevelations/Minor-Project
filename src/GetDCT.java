e2b5b56b5a5ff13ec0c1e3d86dc3adf9789c6dfe
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2015-03-14 16:52:32 +0530

    Extracted DCT coefficients of image

--- src/GetDCT.java
+++ src/GetDCT.java
@@ -1,76 +1,62 @@
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 
 public class GetDCT {
 	public static void main(String args[]) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
 		// Read image
-		File input = new File("C:\\New folder\\cic.png");
-	      BufferedImage image = null;
-		try {
-			image = ImageIO.read(input);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	      
-	      byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-	      Mat img = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
-	      img.put(0, 0, data);
+		Mat img = Imgcodecs.imread("E:\\opencv\\cic.jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
 		 
 		// Make sure the both image dimensions are a multiple of 2
-		Mat img2;
+		Mat img2 = new Mat();
 		int w = img.cols();
 		int h = img.rows();
 		int w2,h2;
-		
-		if (w % 2 == 0) {
+		if (w % 2 == 0)
 		    w2 = w;
-		} else {
+		else
 		    w2 = w+1;
-		}
-		
-		if (h % 2 == 0) {
+		if (h % 2 == 0)
 		    h2 = h;
-		} else {
+		else
 		    h2 = h+1;
-		}
 		
-		copyMakeBorder(img, img2, 0, h2-h, 0, w2-w, IPL_BORDER_REPLICATE);
+		//Core.copyMakeBorder(img, img2, 0, h2-h, 0, w2-w, IPL_BORDER_REPLICATE);
+		Core.copyMakeBorder(img, img2, 0, h2-h, 0, w2-w, 4);
 		 
 		// Grayscale image is 8bits per pixel,
 		// but dct() method wants float values!
 		Mat img3 = new Mat(img2.rows(), img2.cols(), CvType.CV_64F);
 		img2.convertTo(img3, CvType.CV_64F);
-		imwrite("C:\\New Folder\\orig.png", img3);
+		Imgcodecs.imwrite("E:\\opencv\\orig.jpg", img3);
 		 
 		// Let's do the DCT now: image => frequencies
-		Mat freq;
-		dct(img3, freq);
+		Mat freq = new Mat();
+		Core.dct(img3, freq);
 		 
 		// Save a visualization of the DCT coefficients
-		imwrite("W:\\tmp\\dct.png", freq);
+		Imgcodecs.imwrite("E:\\opencv\\temp.jpg", freq);
 		 
-		for (int Start=100; Start>0; Start-=1) {
+		/*for (int Start=100; Start>0; Start-=1)
+		{
 		    // Set some frequencies to 0
-		    for (int y=Start; y<freq.rows; y++) {
-		        for (int x=Start; x<freq.cols; x++) {
-		            freq.at<double>(y,x) = 0.0;
+		    for (int y=Start; y<freq.rows(); y++)
+		    {
+		        for (int x=Start; x<freq.cols(); x++)
+		        {
+		            //freq.at<double>(y,x) = 0.0;
+		            freq.put(y, x, 0.0);
 		        }
 		    }
 		 
 		    // Do inverse DCT: (some low) frequencies => image
 		    Mat dst;
-		    idct(freq, dst);
+		    Core.idct(freq, dst);
 		 
 		    // Show frame nr
 		    char txt[100];
@@ -82,7 +68,7 @@ public class GetDCT {
 		    //we have thrown away some of the high frequencies
 		    char fname[500];
 		    sprintf(fname, "W:\\tmp\\idct_%04d.png", Start);
-		    imwrite(fname, dst);
-		}
+		    Imgcodecs.imwrite(fname, dst);
+		}*/
 	}
 }
